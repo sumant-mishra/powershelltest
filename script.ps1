57eb4549c6c6ac9b41beee346d9575e7c6b97582
Sumanta Kumar Mishra <40567131+sumant-mishra@users.noreply.github.com>
2020-10-27 19:13:42 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,11 +1,1 @@
-Write-Host "Exceuting Powersell Command"
-$location = Get-Location
-Write-Host $location
-C:
-Cd \Users\LocalAdminUser\Automation\Test
-$location = Get-Location
-Write-Host $location
-
-Write-Host "running bat file"
-Get-ChildItem
-Start-Process "cmd.exe" "/c RUN.bat"
+& "C:\script.ps1"
